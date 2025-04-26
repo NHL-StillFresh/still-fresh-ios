d4d2d5fc0222f3ff3a1b8d6ad6583456dec367ac
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2025-04-26 20:59:00 +0200

    Initial Commit

---
+++ Still Fresh/Still_FreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  Still_FreshApp.swift
+//  Still Fresh
+//
+//  Created by Gideon Dijkhuis on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct Still_FreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Still FreshTests/Still_FreshTests.swift
@@ -0,0 +1,17 @@
+//
+//  Still_FreshTests.swift
+//  Still FreshTests
+//
+//  Created by Gideon Dijkhuis on 26/04/2025.
+//
+
+import Testing
+@testable import Still_Fresh
+
+struct Still_FreshTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
