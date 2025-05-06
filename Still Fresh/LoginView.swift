b2bf8945c6d0332158943db52a1cbb509e86380c
Gideon Dijkhuis <dijkhuis.g.a@outlook.com>
2025-05-06 09:57:07 +0200

    Changed backgroundcolor and added extra 'Continue with Email' button

--- Still Fresh/LoginView.swift
+++ Still Fresh/LoginView.swift
@@ -10,18 +10,34 @@ import AuthenticationServices
 
 struct LoginView : View {
     var color = Color("LoginBackgroundColor")
+    var buttonHeight:CGFloat = 50;
     var body: some View {
         ZStack(alignment: .bottomLeading) {
             Color(color)
             VStack() {
-                Text("Your groceries called-")
-                Text("They want to stay fresh")
+                Text("Your groceries called-").colorInvert().font(.title)
+                Text("They want to stay fresh").colorInvert().font(.title)
                 SignInWithAppleButton(.signUp) {
                     request in
                 } onCompletion: {
                     result in
-                }.frame(height: 50)
-                
+                }.frame(height: buttonHeight)
+                Text("or").colorInvert()
+                Button(action: {
+                    
+                }) {
+                    HStack {
+                        Spacer()
+                        Text("Continue with email")
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                    .frame(height: 50)
+                    .background(Color.white)
+                    .foregroundColor(Color("LoginBackgroundColor"))
+                    .cornerRadius(8)
+                }
+
             }.padding()
         }.background(color)
 
