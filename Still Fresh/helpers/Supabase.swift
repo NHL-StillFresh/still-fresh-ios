3d17bc3c74ea2c1297f8ae159b3fa771ad5d5ce2
Jesse van der Voet <166910768+SufficientFlow@users.noreply.github.com>
2025-05-11 02:13:54 +0200

    fix: fixed client name

--- Still Fresh/helpers/Supabase.swift
+++ Still Fresh/helpers/Supabase.swift
@@ -1,7 +1,7 @@
 import Supabase
 import Foundation
 
-let client = SupabaseClient(
+let Supabase = SupabaseClient(
     supabaseURL: URL(string: "https://rfaddfahfpssnxrbxplc.supabase.co")!,
     supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmYWRkZmFoZnBzc254cmJ4cGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MTQ4ODEsImV4cCI6MjA2MjA5MDg4MX0.3cUGivB6XZTWC-7-MrEnatARAWJAap3oC5MCAmqnfo0"
 )
