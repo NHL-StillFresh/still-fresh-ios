b44f14f1e56e25f6c1240120c4ae4f8b067954c4
Jesse van der Voet <166910768+SufficientFlow@users.noreply.github.com>
2025-05-13 10:27:49 +0200

    chore: changed db link

--- Still Fresh/helpers/SupaClient.swift
+++ Still Fresh/helpers/SupaClient.swift
@@ -2,6 +2,6 @@ import Supabase
 import Foundation
 
 let SupaClient = SupabaseClient(
-    supabaseURL: URL(string: "https://rfaddfahfpssnxrbxplc.supabase.co")!,
-    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmYWRkZmFoZnBzc254cmJ4cGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MTQ4ODEsImV4cCI6MjA2MjA5MDg4MX0.3cUGivB6XZTWC-7-MrEnatARAWJAap3oC5MCAmqnfo0"
+    supabaseURL: URL(string: "https://uuhcwgknjekbcsoieuym.supabase.co")!,
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1aGN3Z2tuamVrYmNzb2lldXltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxMjM3ODYsImV4cCI6MjA2MjY5OTc4Nn0.VfJWncP33h2wcEuEWInKirnsbX6rYFWpXE6ipu3xaLU"
 )
