5cae53512dfa673b36bb4d06dddaeb2d563bd5d7
Jesse van der Voet <166910768+SufficientFlow@users.noreply.github.com>
2025-06-19 20:06:38 +0200

    feat: Implement AvatarGenerator for dynamic avatar image URL generation

---
+++ Still Fresh/lib/AvatarGenerator.swift
@@ -0,0 +1,36 @@
+//
+//  UserImageGenerator.swift
+//  Still Fresh
+//
+//  Created by Jesse van der Voet on 19/06/2025.
+//
+
+import Foundation
+
+
+class AvatarGenerator {
+    static let DICEBEAR_URL = "api.dicebear.com"
+    static let DICEBEAR_API_VERSION = "9.x"
+    static let DICEBEAR_IMAGE_FORMAT = "png"
+    
+    enum Category: String {
+        case THUMBS = "thumbs"
+        case EMOJI = "fun-emoji"
+        case BOTTTS = "bottts-neutral"
+    }
+    
+    static func generateAvatarImageURL(
+        withName name: String,
+        category: Category = .THUMBS
+    ) -> URL? {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = DICEBEAR_URL
+        urlComponents.path = "/\(DICEBEAR_API_VERSION)/\(category.rawValue)/\(DICEBEAR_IMAGE_FORMAT)"
+        urlComponents.queryItems = [
+            URLQueryItem(name: "seed", value: name)
+        ]
+        return urlComponents.url
+    }
+}
+
